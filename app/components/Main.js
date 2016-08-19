92bc9a352e38cd5ae5f682496d4e95ec1bf0c554
Tsinat Zeree <cheru87@gmail.com>
2016-08-19 13:21:53 -0700

    players page set

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Main = React.createClass({
     render: function() {
-        reutrn (
+        return (
             <div className='main-container'>
                 {this.props.children}
             </div>
--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = '18a1340ca7484e7fadf92191658a8769'
+var sentryApp = '93220'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+var _APP_INFO = {
+    name: 'Github Battle',
+    branch: '4',
+    version: '1.0'
+}
+
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch
+    }
+}).install()
 
 ReactDOM.render(
     routes,
