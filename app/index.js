2616c34bc6e4bc3167e46d89c05fcc0b35c9a7fc
Tsinat Zeree <cheru87@gmail.com>
2016-08-18 23:36:11 -0700

    react router configured

--- app/index.js
+++ app/index.js
@@ -6,40 +6,9 @@ var USER_DATA = {
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-    render: function(){
-        return <img src = {this.props.imageUrl} style ={{height: 100, width: 100}} />
-    }
-});
-var ProfileLink = React.createClass({
-    render: function() {
-        return (
-            <a href={'https://www.github.com/' + this.props.username }>
-                {this.props.username}
-            </a>
-        )
-    }
-});
-var ProfileName = React.createClass({
-    render: function() {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        )
-    }
-})
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name}/>
-                <ProfileLink   username={this.props.user.username} />
-            </div>
-        )
-    }
-});
-
-ReactDOM.render(<Avatar user={USER_DATA}/>, document.getElementById('app'));
+ReactDOM.render(
+    routes,
+    document.getElementById('app')
+);
