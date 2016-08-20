6842aad25fb27cfbb1304b7ec1bf7cb94d8030d2
Tsinat Zeree <cheru87@gmail.com>
2016-08-20 14:30:28 -0700

    players battle working

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,38 @@ function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function gerRepos(username) {
+    //fetch usernames repos
+    return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+    //calculate all the start that the user has
+    return repos.data.reduce(function(prev, current) {
+        return prev + current.stargazers_count
+    }, 0)
+}
+
+function getPlayersData(player) {
+    //get Repos, getTotalStars, return object with that data
+    return gerRepos(player.login)
+        .then(getTotalStars)
+        .then(function(totalStars) {
+            return {
+                followers: player.followers,
+                totalStars: totalStars
+            }
+        })
+}
+
+function calculateScores(players) {
+    //return an array, after doing some fancy algorithms to determine a winner
+    return [
+        players[0].followers * 3 + players[0].totalStars,
+        players[1].followers * 3 + players[1].totalStars
+    ]
+}
+
 var helpers = {
     getPlayersInfo: function(players) {
         // fetch some date form github
@@ -19,6 +51,16 @@ var helpers = {
                 return user.data;
             })
         })
+    },
+    battle: function(players) {
+        var playerOneData = getPlayersData(players[0]);
+        var playerTwoData = getPlayersData(players[1]);
+
+        return axios.all([playerOneData, playerTwoData])
+            .then(calculateScores)
+            .catch(function(err) {
+                console.warn('Error while getPlayersInfo:', err);
+            })
     }
 };
 
