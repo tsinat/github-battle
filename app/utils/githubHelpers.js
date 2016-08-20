2913fe422b8cec5385ff9930a03559c4be356672
Tsinat Zeree <cheru87@gmail.com>
2016-08-20 13:45:16 -0700

    battle routes working

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,11 +1,11 @@
 var axios = require('axios');
 
-var id = '';
-var sec = '';
+var id = 'f94518856ff6039b43da';
+var sec = 'e64a6a97f3b4faa5feddac48c0ef0781306aa7e6';
 var param = '?client_id=' + id + '&client_secret=' + sec;
 
-function getUserInfo(username) {
-    return axios.get('https://api/github.com/users/' + username + param)
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
 var helpers = {
@@ -13,7 +13,12 @@ var helpers = {
         // fetch some date form github
         return axios.all(players.map(function(username){
             return getUserInfo(username)
-        }))
+        })).then(function(info) {
+            console.log(info);
+            return info.map(function(user) {
+                return user.data;
+            })
+        })
     }
 };
 
